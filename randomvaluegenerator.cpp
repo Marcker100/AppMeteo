14abca628abb9b4ce5fd36a3f2d7e126fa19e3c8
Meja04 <155897981+Meja04@users.noreply.github.com>
2024-06-26 23:16:08 +0200

    Update randomvaluegenerator.cpp

--- randomvaluegenerator.cpp
+++ randomvaluegenerator.cpp
@@ -8,22 +8,27 @@
 
 void RandomValueGenerator::visit(const sensorePolveri& sens) {
     values = generateRandomValues(5.0, 30.0, 24);
+    sens->setData(values);
 }
 
 void RandomValueGenerator::visit(const sensorePressione& sens) {
     values = generateRandomValues(990.0, 1030.0, 24);
+    sens->setData(values);
 }
 
 void RandomValueGenerator::visit(const sensoreTemp& sens) {
     values = generateRandomValues(-10.0, 40.0, 24);
+    sens->setData(values);
 }
 
 void RandomValueGenerator::visit(const sensoreUmid& sens) {
     values = generateRandomValues(20.0, 100.0, 24);
+    sens->setData(values);
 }
 
 void RandomValueGenerator::visit(const sensoreUv& sens) {
     values = generateRandomValues(1.0, 10.0, 24);
+    sens->setData(values);
 }
 
 QVector<double> RandomValueGenerator::generateRandomValues(qreal lowerBound, qreal upperBound, int numValori) {
